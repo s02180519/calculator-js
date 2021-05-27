d22d33843fd7e42ca5f158004bfdcdd8954f281b
s02180519 <72628159+s02180519@users.noreply.github.com>
2021-05-27 15:54:35 +0300

    Add files via upload

---
+++ calculator/app/calculator.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+
+    // VARIABLES
+    var calc = $('.calculator');
+    var calcDisplay = calc.find('.calculator__display');
+    var calcKeys = calc.find('.calculator__key');
+    var calcButton = calc.find('.calculator__button');
+    var calcClear = calc.find('.calculator__clear');
+    var calcEqual = calc.find('.calculator__key--equal');
+    var calcPower = calc.find('.calculator__power');
+    var calcSpace = calc.find('.calculator__backspace');
+
+    calcButton.on('click', function () {
+        alert('Привет мир!');
+        calcDisplay.val(calcDisplay.val() + $(this).attr('value'));
+    });
+    calcClear.on('click', function () {
+        calcDisplay.val('');
+    });
+
+    // SHOW RESULT
+    calcEqual.on('click', function () {
+        calcDisplay.val( eval( calcDisplay.val() ) );
+    });
+
+    // POWER BUTTON
+    calcPower.on('click', function () {
+        calcDisplay.val( Math.pow( calcDisplay.val(), 3 ) );
+    });
+
+    // BACKSPACE BUTTON
+    calcSpace.on('click', function () { // http://www.w3schools.com/jsref/jsref_substring.asp
+        calcDisplay.val( calcDisplay.val().substring(0, calcDisplay.val().length-1) );
+    });
+
+});
